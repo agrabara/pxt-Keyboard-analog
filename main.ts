e78d4245fcad2d40d79b878961a33e546719fe51
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-03-07 13:11:21 +0100

    First version - analog reading of keys

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//Rafal - klawiatura analogowa 5 przyciskow
+
+let arp = 0
+basic.forever(function () {
+    arp = pins.analogReadPin(AnalogPin.P0)
+    if (arp >= 340 && arp <= 370) {  //FIRE
+        led.plot(2, 2)
+    } else if (arp >= 20 && arp <= 50) { //UP
+        led.plot(2, 0)
+    } else if (arp >= 80 && arp <= 100) { //DOWN
+        led.plot(2, 4)
+    } else if (arp >= 0 && arp <= 15) { //LEFT
+        led.plot(0, 2)
+    } else if (arp >= 150 && arp <= 185) { //RIGHT
+        led.plot(4, 2)
+    } else {   // EMPTY
+        led.unplot(2, 2)
+        led.unplot(2, 0)
+        led.unplot(2, 4)
+        led.unplot(0, 2)
+        led.unplot(4, 2)
+    }
+    basic.pause(10)
+})
+ 
